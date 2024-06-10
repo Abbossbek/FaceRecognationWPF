727df82690153fa20c62fadbf657db0ded04f6b5
Abbosbek Niyozqulov <abbos6882@gmail.com>
2024-06-10 14:14:54 +0500

    update

---
+++ FaceRecognationWPF/HelperExtentions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace FaceRecognationWPF.Extentions
+{
+    public static class HelperExtentions
+    {
+        public static BitmapSource ToBitmapSource(this byte[] bytes)
+        {
+            using (var stream = new MemoryStream(bytes))
+            {
+                return BitmapFrame.Create(stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+            }
+        }
+
+    }
+}
--- FaceRecognationWPF/MainWindow.xaml.cs
+++ FaceRecognationWPF/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ using FaceRecognationWPF.Models;
 
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp;
+using FlashCap;
+using System.Reflection.PortableExecutable;
+using FaceRecognationWPF.Extentions;
 
 namespace FaceRecognationWPF
 {
@@ -28,38 +31,63 @@ namespace FaceRecognationWPF
     /// </summary>  
     public partial class MainWindow : System.Windows.Window
     {
+        private IFaceDetector det0;
         private IFaceDetectorWithLandmarks det;
         private IFaceEmbeddingsGenerator rec;
-        UsbCamera camera;
+        private CaptureDevice camera;
         Timer faceTimer;
+        private byte[] lastImageBytes;
         ObservableCollection<Person> persons = new();
         ObservableCollection<Person> savedPersons = new();
         public MainWindow()
         {
             InitializeComponent();
-            var devices = UsbCamera.FindDevices();
-            if (devices.Length == 0) return;
-            var cameraIndex = 1;
-            var format = UsbCamera.GetVideoFormat(cameraIndex).FirstOrDefault();
-
+            det0 = FaceAiSharpBundleFactory.CreateFaceDetector();
             det = FaceAiSharpBundleFactory.CreateFaceDetectorWithLandmarks();
             rec = FaceAiSharpBundleFactory.CreateFaceEmbeddingsGenerator();
 
-            camera = new UsbCamera(cameraIndex, format);
-            camera.PreviewCaptured += Camera_PreviewCaptured;
+            //Console.WriteLine($"Characteristics: {characteristics}");
+
             faceTimer = new Timer(faceTimerCallback, null, TimeSpan.Zero, TimeSpan.FromMilliseconds(200));
             lbPersons.ItemsSource = persons;
             lbSavedPersons.ItemsSource = savedPersons;
 
         }
-        public override void OnApplyTemplate()
+        public async override void OnApplyTemplate()
         {
-            base.OnApplyTemplate();
-            camera.Start();
+            // Capture device enumeration:
+            var devices = new CaptureDevices();
+            var descriptors = devices.EnumerateDescriptors();
+            if (descriptors.Count() == 0)
+            {
+                Console.WriteLine("No camera detected.");
+                return;
+            }
+            var descriptor0 = descriptors.FirstOrDefault(x => x.Characteristics.Length > 0);
+            //Console.WriteLine($"Camera: {JsonSerializer.Serialize(descriptor0)}");
+            var characteristics = descriptor0.Characteristics.LastOrDefault(x => x.PixelFormat != FlashCap.PixelFormats.Unknown);
+            camera = await descriptor0.OpenAsync(
+                characteristics,
+                async bufferScope =>
+                {
+                    // Captured into a pixel buffer from an argument.
+
+                    // Get image data (Maybe DIB/JPEG/PNG):            
+                    lastImageBytes = bufferScope.Buffer.ExtractImage();
+                    //Source?.Dispose();
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (lastImageBytes != null)
+                            image.Source = lastImageBytes.ToBitmapSource();
+                    });
+
+                    // ...
+                });
+            await camera.StartAsync();
         }
-        protected override void OnClosing(CancelEventArgs e)
+        protected async override void OnClosing(CancelEventArgs e)
         {
-            camera.Stop();
+            await camera.StopAsync();
             base.OnClosing(e);
         }
         private void Camera_PreviewCaptured(BitmapSource source)
@@ -72,9 +100,9 @@ namespace FaceRecognationWPF
         }
         private async void faceTimerCallback(object state)
         {
-            var b = camera.GetBitmap();
-            using var img = SixLabors.ImageSharp.Image.Load<Rgb24>(BitmapSourceToByteArray(b));
-            IReadOnlyCollection<FaceDetectorResult> faces = det.DetectFaces(img);
+            if (lastImageBytes == null) return;
+            using var img = SixLabors.ImageSharp.Image.Load<Rgb24>(lastImageBytes);
+            IReadOnlyCollection<FaceDetectorResult> faces = det0.DetectFaces(img);
             if (faces.Count == 0) return;
             var currentPersons = new List<Person>();
             Parallel.ForEach(faces, async face =>
@@ -121,52 +149,52 @@ namespace FaceRecognationWPF
                 }
             });
             // Extract embeddings
-            foreach (var person in currentPersons)
-            {
-                using var image = img.Clone();
-                rec.AlignFaceUsingLandmarks(image, person.FaceRectangle.Landmarks!);
-                var imgBase64 = image.ToBase64String(SixLabors.ImageSharp.Formats.Jpeg.JpegFormat.Instance).Substring(23);
-                person.Image = ByteArrayToBitmapSource(Convert.FromBase64String(imgBase64));
-                person.Embedding = rec.GenerateEmbedding(image);
-                var fp = savedPersons.FirstOrDefault(p => p.Embedding != null && FaceAiSharp.Extensions.GeometryExtensions.Dot(p.Embedding, person.Embedding) >= 0.42);
-                if (fp != null)
-                {
-                    person.Name = fp.Name;
-                    App.Current.Dispatcher.Invoke(() =>
-                    {
-                        TextBlock textBlock = new TextBlock
-                        {
-                            Text = person.Name,
-                            Foreground = Brushes.Red,
-                            FontSize = 20
-                        };
-                        canvas.Children.Add(textBlock);
-                        Canvas.SetLeft(textBlock, (int)person.FaceRectangle.Box.X * (canvas.Width / img.Width));
-                        Canvas.SetTop(textBlock, (int)person.FaceRectangle.Box.Y * (canvas.Height / img.Height) - 20);
-                    });
-                }
-            }
+            //foreach (var person in currentPersons)
+            //{
+            //    using var image = img.Clone();
+            //    rec.AlignFaceUsingLandmarks(image, person.FaceRectangle.Landmarks!);
+            //    var imgBase64 = image.ToBase64String(SixLabors.ImageSharp.Formats.Jpeg.JpegFormat.Instance).Substring(23);
+            //    person.Image = ByteArrayToBitmapSource(Convert.FromBase64String(imgBase64));
+            //    person.Embedding = rec.GenerateEmbedding(image);
+            //    var fp = savedPersons.FirstOrDefault(p => p.Embedding != null && FaceAiSharp.Extensions.GeometryExtensions.Dot(p.Embedding, person.Embedding) >= 0.42);
+            //    if (fp != null)
+            //    {
+            //        person.Name = fp.Name;
+            //        App.Current.Dispatcher.Invoke(() =>
+            //        {
+            //            TextBlock textBlock = new TextBlock
+            //            {
+            //                Text = person.Name,
+            //                Foreground = Brushes.Red,
+            //                FontSize = 20
+            //            };
+            //            canvas.Children.Add(textBlock);
+            //            Canvas.SetLeft(textBlock, (int)person.FaceRectangle.Box.X * (canvas.Width / img.Width));
+            //            Canvas.SetTop(textBlock, (int)person.FaceRectangle.Box.Y * (canvas.Height / img.Height) - 20);
+            //        });
+            //    }
+            //}
 
-            lock (persons)
-            {
-                App.Current.Dispatcher.Invoke(() =>
-                {
-                    foreach (var person in persons.ToList())
-                    {
-                        if (!currentPersons.Contains(person))
-                        {
-                            persons.Remove(person);
-                        }
-                    }
-                    foreach (var person in currentPersons)
-                    {
-                        if (!persons.Contains(person))
-                        {
-                            persons.Add(person);
-                        }
-                    }
-                });
-            }
+            //lock (persons)
+            //{
+            //    App.Current.Dispatcher.Invoke(() =>
+            //    {
+            //        foreach (var person in persons.ToList())
+            //        {
+            //            if (!currentPersons.Contains(person))
+            //            {
+            //                persons.Remove(person);
+            //            }
+            //        }
+            //        foreach (var person in currentPersons)
+            //        {
+            //            if (!persons.Contains(person))
+            //            {
+            //                persons.Add(person);
+            //            }
+            //        }
+            //    });
+            //}
         }
         private static byte[] BitmapSourceToByteArray(BitmapSource source)
         {
